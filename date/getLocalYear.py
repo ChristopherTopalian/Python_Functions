696210e8682e8387c1674eed8c8fcf9e42e34b11
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-15 09:53:29 -0500

    Add files via upload

---
+++ date/getLocalYear.py
@@ -0,0 +1,26 @@
+# getLocalYear.py
+
+import time
+
+def getLocalYear():
+    localYear = time.localtime().tm_year
+    return localYear
+
+####
+
+if __name__ == '__main__':
+    print(getLocalYear())
+    input('')
+
+####
+
+'''
+2024
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
