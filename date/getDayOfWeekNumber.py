66742cd2df935bac63e71dc6545a0d9ee7d480ad
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-15 06:11:28 -0500

    Add files via upload

---
+++ date/getDayOfWeekNumber.py
@@ -0,0 +1,36 @@
+# getDayOfWeekNumber.py
+
+import datetime as dt
+
+def getDayOfWeekNumber():
+    now = dt.datetime.now()
+
+    # sunday returns as 6
+    dayOfWeekNumber = now.weekday()
+
+    # adjust so that sunday returns as 0
+    dayOfWeekNumber = (dayOfWeekNumber + 1) % 7
+
+    # adjust so that sunday returns as 1
+    dayOfWeekNumberAdjusted = dayOfWeekNumber + 1
+
+    return dayOfWeekNumberAdjusted
+
+##
+
+if __name__ == '__main__':
+    print(getDayOfWeekNumber())
+    input('')
+
+'''
+4, if wednesday
+'''
+
+##
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
