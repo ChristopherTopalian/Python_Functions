e9a241a91b277545d083afd8713ab97ff3ff9a00
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-15 09:43:10 -0500

    Add files via upload

---
+++ date/getLocalDay.py
@@ -0,0 +1,26 @@
+# getLocalDay.py
+
+import time
+
+def getLocalDay():
+    localDay = time.localtime().tm_mday
+    return localDay
+
+####
+
+if __name__ == '__main__':
+    print(getLocalDay())
+    input('')
+
+####
+
+'''
+22
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
