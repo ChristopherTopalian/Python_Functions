a73977b12cd581ebe4750d012f3d95d32871d82d
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-15 08:12:05 -0500

    Add files via upload

---
+++ date_time/getAscDateTime.py
@@ -0,0 +1,26 @@
+# getAscDateTime.py
+
+import time
+
+def getAscDateTime():
+    currentDateTime = time.asctime()
+    return currentDateTime
+
+####
+
+if __name__ == '__main__':
+    print(getAscDateTime())
+    input('')
+
+####
+
+'''
+Fri Nov 15 07:51:52 2024
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
