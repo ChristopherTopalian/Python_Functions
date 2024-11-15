6959dd5132850868a517cf4104906b3e8fb76c5b
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-15 12:04:36 -0500

    Add files via upload

---
+++ time/getTime12.py
@@ -0,0 +1,28 @@
+# getTime12.py
+
+import datetime as dt
+
+def getTime12():
+    currentTime = dt.datetime.now()
+
+    formattedTime = currentTime.strftime("%I:%M:%S %p")
+
+    return formattedTime
+
+####
+
+if __name__ == '__main__':
+    print(getTime12())
+    input('')
+
+####
+
+'''
+05:43:07 PM
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+
