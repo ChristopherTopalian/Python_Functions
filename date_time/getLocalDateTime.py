cfb439847da8e680377c0eb231ac5f0ce4ac3639
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-15 13:38:19 -0500

    Add files via upload

---
+++ date_time/getLocalDateTime.py
@@ -0,0 +1,27 @@
+# getLocalDateTime.py
+
+import time
+
+def getLocalDateTime():
+    localDateTime = time.localtime()
+    return localDateTime
+
+####
+
+if __name__ == '__main__':
+    print(getLocalDateTime())
+    input('')
+
+####
+
+'''
+time.struct_time(tm_year=2024, tm_mon=11, tm_mday=15, tm_hour=13, tm_min=7, 
+tm_sec=16, tm_wday=4, tm_yday=320, tm_isdst=0)
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
