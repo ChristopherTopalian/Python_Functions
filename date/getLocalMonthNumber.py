7a25c505133a436f013c3b5e25a7e630dcb81dc0
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-15 09:48:31 -0500

    Add files via upload

---
+++ date/getLocalMonthNumber.py
@@ -0,0 +1,26 @@
+# getLocalMonthNumber.py
+
+import time
+
+def getLocalMonthNumber():
+    localMonth = time.localtime().tm_mon
+    return localMonth
+
+####
+
+if __name__ == '__main__':
+    print(getLocalMonthNumber())
+    input('')
+
+####
+
+'''
+11
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
