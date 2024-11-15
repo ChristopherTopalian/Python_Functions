96e941e38331f33510098c0e370602d5f9fc31db
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-15 03:14:56 -0500

    Add files via upload

---
+++ date/getDayOfMonth.py
@@ -0,0 +1,27 @@
+# getDayOfMonth.py
+
+import datetime as dt
+
+def getDayOfMonth():
+    currentDate = dt.datetime.now()
+    dayNumber = currentDate.day
+    return dayNumber
+
+##
+
+if __name__ == '__main__':
+    print(getDayOfMonth())
+    input('')
+
+####
+
+'''
+17
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
