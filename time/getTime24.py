a2a6aba46b938b4b9c3ec6e0a6e4062f26a4ae59
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-15 11:56:54 -0500

    Add files via upload

---
+++ time/getTime24.py
@@ -0,0 +1,28 @@
+# getTime24.py
+
+import datetime as dt
+
+def getTime24():
+    currentTime = dt.datetime.now()
+
+    formattedTime = currentTime.strftime('%H:%M:%S')
+
+    return formattedTime
+
+####
+
+if __name__ == '__main__':
+    print(getTime24())
+    input('')
+
+####
+
+'''
+17:07:29
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+
