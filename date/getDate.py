079b5c5735e4524ffdf9d4e03312d7f8eccb69c6
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-14 19:22:37 -0500

    Add files via upload

---
+++ date/getDate.py
@@ -0,0 +1,26 @@
+# getDate.py
+
+import datetime as dt
+
+def getDate():
+    today = dt.date.today()
+    return today
+
+##
+
+if __name__ == "__main__":
+    print(getDate())
+    input('')
+
+####
+
+'''
+2024-10-18
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
