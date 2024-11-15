dda918a9429d73f246142852ed6e896fca01b5f6
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-15 05:44:43 -0500

    Add files via upload

---
+++ date/getDayOfWeekName.py
@@ -0,0 +1,35 @@
+# getDayOfWeekName.py
+
+import datetime as dt
+
+def getDayOfWeekName():
+    currentDate = dt.datetime.now()
+
+    # 0 is Monday, 6 is Sunday
+    dayOfWeek = currentDate.weekday()
+
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    # get name of day using day of week index
+    dayName = days[dayOfWeek]
+
+    return dayName
+
+##
+
+if __name__ == '__main__':
+    print(getDayOfWeekName())
+    input('')
+
+####
+
+'''
+Monday
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
